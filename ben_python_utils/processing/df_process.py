14f33d2d44578b867887acaf61f2197b660bfb6e
김현중 <hj.ben.kim@gmail.com>
2023-05-31 10:35:50 +0900

    Update df_process.py
    some typo fix / add split_index function

--- ben_python_utils/processing/df_process.py
+++ ben_python_utils/processing/df_process.py
@@ -59,6 +59,37 @@ def merge_df(root_dir):
     )
 
 
+def get_split_index(data, split_n):
+    """
+        Get even-split indexes for a list-like object.
+
+        Raises
+        ------
+        TypeError
+            If the split_n is not an int.
+
+        Parameters
+        ----------
+        data : List-like objects
+            Target Data (required)
+
+        split_n : Integer
+            The number of split (required)
+
+        Returns
+        -------
+        List
+            Index list to split data
+    """
+    if type(split_n) != int:
+        raise TypeError("Type of target directory name must be <class 'int'>, but {}".format(type(split_n)))
+
+    if isinstance(data, dict):
+        return [int(len(list(data.keys())) * (i + 1) / split_n) for i in range(split_n - 1)]
+    else:
+        return [int(len(data) * (i + 1) / split_n) for i in range(split_n - 1)]
+
+
 def convert_str_columns_to_datetime(df, columns, datetime_format):
     """
         Convert a str-formatted DataFrame column to datetime type
@@ -67,7 +98,7 @@ def convert_str_columns_to_datetime(df, columns, datetime_format):
         ------
         TypeError
             If the df is not a DataFrame.
-            If column type is not a str or list.
+            If the type of columns is not a str or list.
 
         Parameters
         ----------
@@ -91,7 +122,7 @@ def convert_str_columns_to_datetime(df, columns, datetime_format):
     if type(columns) == str:
         columns = [columns]
     elif type(columns) != list:
-        raise TypeError("Type of target df name must be <class 'str'> or <class 'list'>, but {}".format(type(df)))
+        raise TypeError("Type of target column name must be <class 'str'> or <class 'list'>, but {}".format(type(df)))
 
     for column in columns:
         df.loc[:, column] = pd.to_datetime(df[column], format=datetime_format)
