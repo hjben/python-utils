424e2fdcd69b252c3ad52d273de98cc45b1aa892
김현중 <hj.ben.kim@gmail.com>
2023-05-31 12:47:27 +0900

    Update df_process.py
    add genarate_dummy, drop_column function / typo fix

--- ben_python_utils/processing/df_process.py
+++ ben_python_utils/processing/df_process.py
@@ -2,6 +2,55 @@ import os
 import pandas as pd
 
 
+def check_df(df):
+    """
+    Check if the input dataframe has valid type
+
+    Raises
+    ------
+    ValueError
+         If the df is not a DataFrame.
+
+    Parameters
+    ----------
+    df : DataFrame
+        Target dataframe (required)
+
+    Returns
+    -------
+    None
+    """
+    if type(df) != pd.core.frame.DataFrame:
+        raise TypeError("Type of target df name must be <class 'pandas.core.frame.DataFrame'>, but {}".format(type(df)))
+
+
+def check_column(columns):
+    """
+    Check if the input columns have valid type
+
+    Raises
+    ------
+    ValueError
+         If the type of columns is not a str or list.
+
+    Parameters
+    ----------
+    columns : String or List
+        Target column(s) of dataframe (required)
+
+    Returns
+    -------
+    List
+        Columns list
+    """
+    if type(columns) == str:
+        columns = [columns]
+    elif type(columns) != list:
+        raise TypeError("Type of target column name must be <class 'str'> or <class 'list'>, but {}".format(type(columns)))
+
+    return columns
+
+
 def get_all_duplicate(df, column_list):
     """
     Get all duplicated rows.
@@ -48,7 +97,7 @@ def merge_df(root_dir):
     Returns
     -------
     DataFrame
-        Merged DataFrame
+        Merged dataframe
     """
     if type(root_dir) != str:
         raise TypeError("Type of target directory name must be <class 'str'>, but {}".format(type(root_dir)))
@@ -59,9 +108,73 @@ def merge_df(root_dir):
     )
 
 
-def convert_str_columns_to_datetime(df, columns, datetime_format):
+def convert_str_column_to_datetime(df, columns, datetime_format):
     """
         Convert a str-formatted DataFrame column to datetime type
+
+        Raises
+        ------
+        TypeError
+            If the df is not a dataframe.
+            If the type of columns is not a str or list.
+
+        Parameters
+        ----------
+        df : DataFrame
+            Target dataframe (required)
+
+        columns : String or List
+            Target column(s) of dataframe (required)
+
+        datetime_format : String
+            Datetime format to convert (required)
+
+        Returns
+        -------
+        DataFrame
+            Column-converted dataframe
+    """
+    check_df(df)
+    for column in check_column(columns):
+        df.loc[:, column] = pd.to_datetime(df[column], format=datetime_format)
+
+    return df
+
+
+def generate_dummy(df, columns):
+    """
+        Generate dummy from some columns
+
+        Raises
+        ------
+        TypeError
+            If the df is not a dataframe.
+            If the type of columns is not a str or list.
+
+        Parameters
+        ----------
+        df : DataFrame
+            Target dataframe (required)
+
+        columns : String or List
+            Target column(s) of dataframe to generate (required)
+
+        Returns
+        -------
+        DataFrame
+            Dataframe with dummy columns only
+    """
+    check_df(df)
+    dummy_df = pd.DataFrame()
+    for column in check_column(columns):
+        dummy_df = pd.concat([dummy_df, pd.get_dummies(df[column], drop_first=True)], axis=1)
+
+    return dummy_df
+
+
+def drop_column(df, columns):
+    """
+        Drop some columns from a DataFrame
 
         Raises
         ------
@@ -75,25 +188,16 @@ def convert_str_columns_to_datetime(df, columns, datetime_format):
             Target DataFrame (required)
 
         columns : String or List
-            Target column(s) of DataFrame (required)
-
-        datetime_format : String
-            Datetime format to convert (required)
+            Target column(s) of DataFrame to drop (required)
 
         Returns
         -------
         DataFrame
-            Column-converted DataFrame
+            Column-dropped DataFrame
     """
-    if type(df) != pd.core.frame.DataFrame:
-        raise TypeError("Type of target df name must be <class 'pandas.core.frame.DataFrame'>, but {}".format(type(df)))
-
-    if type(columns) == str:
-        columns = [columns]
-    elif type(columns) != list:
-        raise TypeError("Type of target column name must be <class 'str'> or <class 'list'>, but {}".format(type(df)))
-
-    for column in columns:
-        df.loc[:, column] = pd.to_datetime(df[column], format=datetime_format)
+    check_df(df)
+    for column in check_column(columns):
+        if column in df.columns:
+            df = df.drop(column, axis=1)
 
     return df
