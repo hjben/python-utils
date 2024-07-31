f32deea0895c85157f01af1749b8be3fee9944f3
김현중 <hj.ben.kim@gmail.com>
2024-07-31 13:16:59 +0900

    Create array.py
    add describe_array

---
+++ ben_python_utils/processing/array.py
@@ -0,0 +1,32 @@
+"""
+This module provide some utilities to manipulate with numpy array.
+
+Functions:
+    - describe_array: show summary informations.
+"""
+import numpy as np
+
+def describe_array(arr:np.ndarray, name=None):
+    """
+    Show summary informations of an array.
+
+    Parameters
+    ----------
+    arr : NDArray
+        Target array to describe (required)
+
+    name : str
+        Array name
+
+    Returns
+    -------
+    None
+    """
+    if name is not None:
+        print(f"========{name}========")
+
+    print(f"Type: {type(arr)}")
+    print(f"Shape: {arr.shape}")
+    print(f"Min: {arr.min()}")
+    print(f"Max: {arr.max()}")
+    print(f"Values: \n{arr}")
